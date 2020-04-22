fa6dee81348cc81c3a24b6715424bb0ed19f907c
Giosevid <giosevid@gmail.com>
2020-04-22 17:06:11 -0300

    Example test tdd in basic component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      counter: 0,
+      error: false
+    }
+  }
+
+  incrementCounter = () => {
+    if (this.state.error) this.setState({ error: false })
+    this.setState({ counter: this.state.counter + 1 })
+  }
+
+  decrementCounter = () => {
+    if(this.state.counter === 0) {
+      this.setState({ error: true })
+    } else {
+      this.setState({counter: this.state.counter - 1})
+    }
+  }
+
+  render() {
+    return (
+      <div data-test="component-app">
+        <h1 data-test="counter-display">This is counter currently { this.state.counter }</h1>
+        <h2 data-test="error-message" style={{ color: 'red' }}>{this.state.error && 'The counter cannot go below 0'}</h2>
+        <button onClick={this.incrementCounter} data-test="increment-button">Increment counter</button>
+        <button onClick={this.decrementCounter} data-test="decrement-button">Decrement Counter</button>
+      </div>
+    );
+  }
 }
 
 export default App;
